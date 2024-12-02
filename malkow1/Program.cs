70916d0d365ee820964fd206151385b5b3184c4e
ooagu <ooagu@DESKTOP-TII5S3M>
2024-12-02 15:08:58 +0500

    Добавьте файлы проекта.

---
+++ malkow1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Это пример текста. Текст должен быть проанализирован. Это важно.";
+
+        // вывожу результат подсчёта частоты слов
+        foreach (var item in WordFrequency(text))
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+
+    /// <summary>
+    static Dictionary<string, int> WordFrequency(string text)
+    {
+        // убираем знаки препинания
+        var words = text
+            .ToLower()
+            .Split(new[] { ' ', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries); // убираю знаки препинания и пустые строки
+
+        
+        return words
+            .GroupBy(word => word) // группируем
+            .ToDictionary(group => group.Key, group => group.Count()); // преобразуем в словарь где слово - количество
+    }
+}
